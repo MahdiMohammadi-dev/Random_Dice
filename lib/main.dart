d2dc1d329695fe64c6ab90352f9b327b8efa993c
MahdiMohammadi-dev <jnsm89@yahoo.com>
2022-08-27 17:22:46 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+void main() => runApp(const MyStateFulWidget());
+
+class MyStateFulWidget extends StatefulWidget {
+  const MyStateFulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStateFulWidget> createState() => _MyStateFulWidgetState();
+}
+
+class _MyStateFulWidgetState extends State<MyStateFulWidget> {
+  int leftdicenumber = 1;
+  int rightdicenumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
+            child: Scaffold(
+          backgroundColor: Colors.red,
+          appBar: AppBar(
+            backgroundColor: const Color.fromARGB(255, 25, 8, 180),
+            title: const Center(
+              child: Text(
+                "Dice-Mahdi Mohammadi-Dev",
+                style: TextStyle(fontWeight: FontWeight.w700),
+              ),
+            ),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.only(top: 200, left: 15, right: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: InkWell(
+                    onTap: (() {
+                      setState(() {
+                        RandomDice();
+                      });
+                    }),
+                    child: Image.asset("assets/images/dice$leftdicenumber.png"),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: InkWell(
+                    onTap: (() {
+                      setState(() {
+                        RandomDice();
+                      });
+                    }),
+                    child:
+                        Image.asset("assets/images/dice$rightdicenumber.png"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )));
+  }
+
+  void RandomDice() {
+    rightdicenumber = Random().nextInt(6) + 1;
+    leftdicenumber = Random().nextInt(6) + 1;
+  }
+}
+
